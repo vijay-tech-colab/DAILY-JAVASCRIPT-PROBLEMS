ccd178386f1dba2efd950de07c409a3192b74f14
vijay kumar <vijaykumar06232@gmail.com>
2024-09-26 08:33:26 +0530

    js practice Day4

---
+++ Day4.js
@@ -0,0 +1,15 @@
+// write a function to sort an array of number in accending order ? 
+// without changing original array 
+
+const accendingOrder = (arr) => {
+    const sortedArr = arr.toSorted((a,b) => a-b);
+    return sortedArr
+}
+
+const num = [2,3,4,5,32,1,674,8,0,7];
+console.log(accendingOrder(num));
+
+
+
+
+
