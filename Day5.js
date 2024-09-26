0ce4497480e6b5654a07929b183b94391abcf2ad
vijay kumar <vijaykumar06232@gmail.com>
2024-09-26 09:11:50 +0530

    js practice Day5

---
+++ Day5.js
@@ -0,0 +1,27 @@
+//! write a function to determine whether a given string is a palindrome ?
+
+//first way using core concept
+const isPalindrome = (str) => {
+    let reveseStr = '';
+    for(let i = str.length-1; i >= 0; i--){
+        reveseStr = reveseStr + str[i];
+    }
+    return reveseStr === str ? true : false
+}
+
+console.log(isPalindrome("vijay")); // false
+console.log(isPalindrome("mom"));   // true
+
+
+//2nd way to using method 
+const isPalindrome = (str) => {
+    const reveseStr = str.split('').reverse().join('');
+    return reveseStr === str ? true : false
+}
+console.log(isPalindrome("vijay")); // false
+console.log(isPalindrome("mom"));   // true
+
+
+
+
+
