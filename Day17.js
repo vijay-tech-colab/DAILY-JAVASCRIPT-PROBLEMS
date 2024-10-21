7e262d246b0a6574fcd8e37a6dea3ce8fe55d359
vijay kumar <vijaykumar06232@gmail.com>
2024-10-21 18:13:05 +0530

    js practice Day17

---
+++ Day17.js
@@ -0,0 +1,25 @@
+// Write a JavaScript function removeDuplicates(arr) that takes an array of numbers as input and returns a new array with all the duplicates removed, preserving the original order of elements.
+
+//Example Input:
+// const removeDuplicates = (arr) => {
+//     return [...new Set(arr)];
+// }
+
+
+// console.log(removeDuplicates([1, 2, 3, 2, 4, 3, 5, 6, 1]));
+
+//Expected Output:
+[1, 2, 3, 4, 5, 6]
+
+// Solution 2: Using a traditional loop
+const removeDuplicates = (arr) => {
+    const uniqueArray = [];
+    for(let i = 0; i < arr.length; i++){
+        if(!uniqueArray.includes(arr[i])){
+            uniqueArray.push(arr[i]);
+        }
+    }
+    return uniqueArray
+}
+
+console.log(removeDuplicates([1, 2, 3, 2, 4, 3, 5, 6, 1]));
