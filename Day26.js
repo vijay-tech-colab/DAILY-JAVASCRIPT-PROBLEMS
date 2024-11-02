f95ff45131407af46c72253755539502c311ccd5
vijay kumar <vijaykumar06232@gmail.com>
2024-11-02 08:56:18 +0530

    js practice Day26

--- Day26.js
+++ Day26.js
@@ -3,21 +3,21 @@
 
 
 // 1st case: using for loop
-// const hasUniqueChars = function (str) {
-//     const lowercase = str.toLowerCase();    
-//     for (let i = 0; i < lowercase.length; i++) {
-//         if(lowercase.charAt(i) === lowercase.charAt(i + 1)) {
-//             return false;
-//         }
-//     }
-//     return true
-// };
+const hasUniqueChars = function (str) {
+    const lowercase = str.toLowerCase();    
+    for (let i = 0; i < lowercase.length; i++) {
+        if(lowercase.charAt(i) === lowercase.charAt(i + 1)) {
+            return false;
+        }
+    }
+    return true
+};
 
 // Test Cases:
-// console.log(hasUniqueChars('123455')); // false because it's not unique 5 characters repeated
-// console.log(hasUniqueChars("hello")); // false, because "l" appears twice
-// console.log(hasUniqueChars("world")); // true, all characters are unique
-// console.log(hasUniqueChars("JavaScript")); // false, because "a" appears twice
+console.log(hasUniqueChars('123455')); // false because it's not unique 5 characters repeated
+console.log(hasUniqueChars("hello")); // false, because "l" appears twice
+console.log(hasUniqueChars("world")); // true, all characters are unique
+console.log(hasUniqueChars("JavaScript")); // false, because "a" appears twice
 
 // 2nd case: using Set 
 
